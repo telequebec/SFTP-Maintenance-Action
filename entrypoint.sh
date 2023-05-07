ef56b1c2f863e416ddad25f757dc63e79277c6ca
Alfred Dagenais <jesuis@alfreddagenais.com>
2023-05-06 23:27:29 -0400

    ADD: folder $REMOTE_PATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,11 +67,11 @@ REMOTE_PATH="$1"
 find "$REMOTE_PATH" -type f
 
 # Liste des dossiers
-find "$REMOTE_PATH" -type d' > /list_files.sh
-    chmod +x /list_files.sh
+find "$REMOTE_PATH" -type d' > list_files.sh
+    chmod +x list_files.sh
 
     # Téléchargement du script list_files.sh sur le serveur distant
-    printf "%s\n" "put /list_files.sh" > $TEMP_SFTP_FILE
+    printf "%s\n" "put list_files.sh $REMOTE_PATH" > $TEMP_SFTP_FILE
     SSHPASS=$SSHPASS sshpass -e sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $PORT -o StrictHostKeyChecking=no $USER@$HOST
 
     # Exécution du script list_files.sh sur le serveur distant et récupération des résultats
